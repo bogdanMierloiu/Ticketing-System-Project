b4ed9c9cffdc874949b43847d959f45689e52f53
bogdan <bogdy.musiclover@gmail.com>
2023-05-19 15:54:53 +0300

    select departments specific for police structure when add request

--- request-web/src/main/resources/static/js/script.js
+++ request-web/src/main/resources/static/js/script.js
@@ -2,11 +2,11 @@
             var policeStructureId = $('#policeStructure').find(':selected').val();
 
             // Efectuează solicitarea către server pentru a obține departamentele
-            fetch('admin/show-departments/' + policeStructureId)
+            fetch('/request/admin/show-departments/' + policeStructureId)
                 .then(response => response.text())
                 .then(html => {
                     // Inserează răspunsul HTML în containerul departamentelor
-                    $('#departmentContainer').html(html);
+                    $('#departmentDropdown').html(html);
                 })
                 .catch(error => {
                     console.log('A apărut o eroare în solicitarea AJAX: ' + error);
